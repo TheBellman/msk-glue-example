3062fca83346233c74760f4615c0d9d25a19faa4
Robert Hook <rahook@gmail.com>
2022-12-31 12:34:59 +0000

    Tweak README and break up Terraform files.

--- terraform/README.md
+++ terraform/README.md
@@ -1,5 +1,12 @@
 # Example
-This example uses the VPC module to stand up a VPC with private and "public" subnets.
+This example uses the VPC module to stand up a VPC with private and "public" subnets. It then:
+
+ - creates a Provisioned MSK cluster in the private subnets;
+ - creates an EC2 instance in a public subnet which can access the cluster;
+ 
+The EC2 instance allows traffic incoming from anywhere on 80 and 443 (e.g. you could mount a web service there), plus 22 for SSH from nominated CIDR blocks.
+
+The cluster is not directly accessible from outside the VPC. This is by design.
 
 ## Prerequisites
 This module does make use of Terraform version constraints (see `versions.tf`) but can be summarised as:
@@ -15,16 +22,24 @@ The example code broadly assumes AWS CLI 2.9.10 or better is available.
 First, create `terraform.tfvars` using `terraform.tfvars.template` as an example, e.g.:
 
 ```
-aws_region     = "eu-west-1"
+aws_region     = "eu-west-2"
 aws_profile    = "adm_rhook_cli"
-aws_account_id = "889199313043"
+aws_account_id = "829199347043"
 vpc_cidr       = "172.21.0.0/16"
 vpc_name       = "msk-glue"
-ssh_inbound    = ["188.211.160.179/32"]
+key_pair_name  = "adm_rhook"
+ssh_inbound    = ["188.211.160.179/32", "3.8.37.24/29"]
 tags           = { Class = "Development" }
 ```
 
-In this example we allow SSH from a particular client.
+In this example we allow SSH from a particular client plus some additional CIDR blocks from AWS.  These additional blocks are needed to allow the use of [Instance Connect](https://aws.amazon.com/about-aws/whats-new/2019/06/introducing-amazon-ec2-instance-connect/) through the AWS console.
+
+These CIDR blocks are available from AWS using a small tool I wrote (see https://github.com/TheBellman/cidrapi):
+
+```
+curl https://mqciw5p4x8.execute-api.eu-west-2.amazonaws.com/v1/cidr/eu-west-2/EC2_INSTANCE_CONNECT
+```
+
 
 After creating `terraform.tfvars` you will need to update `backend.tf` - see the [Terraform Documentation](https://www.terraform.io/docs/backends/index.html) for more information - you can even remove `backend.tf` competely to keep the Terraform state locally.
 
@@ -64,6 +79,64 @@ You can connect to the instance using [mssh](https://github.com/aws/aws-ec2-inst
 
 ```shell
 $ mssh -u adm_rhook_cli i-0135dd236aec248b7
+```
+
+or using SSH with the key pair you have provided:
+
+```shell
+$ ssh -i adm_rhook ec2-user@18.133.161.68 
+```
+
+Note as well that you can get files on and off that host using `scp`:
+
+```shell
+$ scp -i adm_rhook README.md ec2-user@18.133.161.68:README.md
+```
+
+### Using Kafka
+Having created the environment and Kafka cluster, you can exercise the cluster using the inbuilt scripts. As part of the setup, the relevant version of Kafka has been installed in the `ec2-user` home directory for convenient access to the tools. The configuration of the cluster we've setup does not require any client authentication, but it _does_ use TLS for transport between clients and the cluster. This requires a client-side configuration:
+
+```shell
+$ cat client.properties
+security.protocol=SSL
+```
+
+
+With that in hand, and a bootstrap server, we can list topics:
+
+```shell
+$ kafka_2.12-3.2.0/bin/kafka-topics.sh \
+  --bootstrap-server b-1.glueexample.d0h8ge.c4.kafka.eu-west-2.amazonaws.com:9094 \
+  --command-config client.properties \
+  --list
+```
+
+Create a topic:
+
+```shell
+$ kafka_2.12-3.2.0/bin/kafka-topics.sh \
+  --bootstrap-server b-1.glueexample.d0h8ge.c4.kafka.eu-west-2.amazonaws.com:9094 \
+  --command-config client.properties \
+  --create --replication-factor 3 --partitions 1 \
+  --topic TestOne 
+```
+
+Consume from the topic:
+
+```shell
+$ kafka_2.12-3.2.0/bin/kafka-console-consumer.sh \
+	--bootstrap-server b-1.glueexample.d0h8ge.c4.kafka.eu-west-2.amazonaws.com:9094 \
+	--consumer.config client.properties \
+	--topic TestOne
+```
+
+And produce to the topic:
+
+```shell
+$ kafka_2.12-3.2.0/bin/kafka-console-producer.sh \
+	--bootstrap-server b-1.glueexample.d0h8ge.c4.kafka.eu-west-2.amazonaws.com:9094 \
+	--producer.config client.properties \
+	--topic TestOne
 ```
 
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -12,113 +12,6 @@ module "vpc" {
 
 
 # --------------------------------------------------------------------------------
-# security group to be used by the cluster to allow incoming requests
-# --------------------------------------------------------------------------------
-resource "aws_security_group" "dev" {
-  name        = "msk_dev"
-  vpc_id      = module.vpc.vpc_id
-  description = "Security group for MSK cluster"
-  tags        = { Name = "msk_dev" }
-}
-
-resource "aws_security_group_rule" "dev_kafka_in" {
-  security_group_id = aws_security_group.dev.id
-  type              = "ingress"
-  from_port         = 9092
-  to_port           = 9098
-  protocol          = "tcp"
-  cidr_blocks       = tolist(module.vpc.public_subnet)
-}
-
-resource "aws_security_group_rule" "dev_zk_in" {
-  security_group_id = aws_security_group.dev.id
-  type              = "ingress"
-  from_port         = 2181
-  to_port           = 2182
-  protocol          = "tcp"
-  cidr_blocks       = tolist(module.vpc.public_subnet)
-}
-
-# --------------------------------------------------------------------------------
-# the security group for instances needs to allow kafka requests out
-# --------------------------------------------------------------------------------
-
-resource "aws_security_group_rule" "dev_kafka_out" {
-  security_group_id = module.vpc.public_sg
-  type              = "egress"
-  from_port         = 9082
-  to_port           = 9098
-  protocol          = "tcp"
-  cidr_blocks       = tolist(module.vpc.private_subnet)
-}
-
-resource "aws_security_group_rule" "dev_zk_out" {
-  security_group_id = module.vpc.public_sg
-  type              = "egress"
-  from_port         = 2181
-  to_port           = 2182
-  protocol          = "tcp"
-  cidr_blocks       = tolist(module.vpc.private_subnet)
-}
-
-# --------------------------------------------------------------------------------
-# key used for encryption at rest
-# --------------------------------------------------------------------------------
-resource "aws_kms_key" "dev" {
-  description             = "MSK encryption at rest"
-  deletion_window_in_days = 7
-  tags                    = { Name = "msk_demo" }
-}
-
-resource "aws_kms_alias" "dev" {
-  name          = "alias/msk"
-  target_key_id = aws_kms_key.dev.key_id
-}
-
-# --------------------------------------------------------------------------------
-# bucket for logging 
-# --------------------------------------------------------------------------------
-resource "aws_s3_bucket" "logs" {
-  bucket_prefix = "kafka-logs-"
-
-  force_destroy = true
-
-  tags = { "Name" = "Kafka Logs" }
-}
-
-resource "aws_s3_bucket_acl" "logs" {
-  bucket = aws_s3_bucket.logs.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "logs" {
-  bucket = aws_s3_bucket.logs.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "logs" {
-  bucket = aws_s3_bucket.logs.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-# --------------------------------------------------------------------------------
-# log group  
-# --------------------------------------------------------------------------------
-resource "aws_cloudwatch_log_group" "dev" {
-  name_prefix = "kafka_"
-
-  retention_in_days = 7
-  tags              = { Name = "Kafka" }
-}
-
-
-# --------------------------------------------------------------------------------
 # create the MSK cluster 
 # --------------------------------------------------------------------------------
 resource "aws_msk_configuration" "dev" {
@@ -132,13 +25,18 @@ PROPERTIES
 }
 
 resource "aws_msk_cluster" "dev" {
-  cluster_name           = local.cluster_name
-  kafka_version          = local.kafka_version
+  cluster_name  = local.cluster_name
+  kafka_version = local.kafka_version
+  # One broker in each AZ
   number_of_broker_nodes = length(data.aws_availability_zones.available.zone_ids)
 
   broker_node_group_info {
-    instance_type   = local.cluster_node_type
-    client_subnets  = tolist(module.vpc.private_subnet_id)
+    instance_type = local.cluster_node_type
+
+    # This specifies what subnets the cluster will be built into
+    client_subnets = tolist(module.vpc.private_subnet_id)
+
+    # This is the security group that controls where requests can come from
     security_groups = [aws_security_group.dev.id]
 
     # without specifying a client_authentication block, there's no client authentication required
